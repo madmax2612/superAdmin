776a0c7b7057c84886c8054f9e26200db135061e
yagya <prakashyagya@gmail.com>
2020-09-02 23:58:31 +0530

    live login integration

--- src/Components/LoginPage/LoginPage.js
+++ src/Components/LoginPage/LoginPage.js
@@ -1,54 +1,102 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import Paper from "@material-ui/core/Paper";
 import TextField from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import InputAdornment from "@material-ui/core/InputAdornment";
-import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import PhoneAndroidIcon from "@material-ui/icons/PhoneAndroid";
+// import MailOutlineIcon from "@material-ui/icons/MailOutline";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import LockIcon from "@material-ui/icons/Lock";
 import { useStyles } from "./css";
 import { useForm, Controller } from "react-hook-form";
-import { Link, Router } from "react-router-dom";
+import LoginModal from "../../util/LoginModal/LoginModal";
+import { UserLogin } from "../../util/Services/Services";
+import { Link, HashRouter, Redirect } from "react-router-dom";
 
 export const baseUrl = "/otopark/superadmin";
 
-export default function SignIn() {
+export default function SignIn(props) {
   const history = useHistory();
   const classes = useStyles();
-  const { handleSubmit, reset, watch, control, register } = useForm();
+  const { handleSubmit, reset, watch, control, register, errors } = useForm();
   const [redirect, setRedirect] = useState(false);
   const [error, setError] = useState(false);
-  const onSubmit = data => {
-    console.log(data);
-    if (data.password && data.email) {
-      history.push(baseUrl + "/dashboard");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    if (sessionStorage.getItem("payload")) {
       setRedirect(true);
-    } else {
-      setError(true);
+      setError(false);
+    }
+  }, []);
+
+  console.log(error);
+
+  const onSubmit = (data, e) => {
+    e.preventDefault();
+    console.log(data);
+
+    //    setStoreRanger(data.RangerID)
+    // console.log(data);
+    if (data) {
+      let formData = new FormData();
+      formData.append("mobile", data.mobile);
+      formData.append("password", data.password);
+      setError(false);
+
+      UserLogin(formData)
+        .then(res => {
+          console.log(res);
+          if (res.data.message == "Success") {
+            console.log("inside success");
+            setError(false);
+            sessionStorage.setItem("payload", JSON.stringify(res.data));
+            setRedirect(true);
+          } else {
+            setRedirect(false);
+            setError(true);
+            setErrorMessage(res.data.message);
+          }
+        })
+        .catch(err => {
+          console.log(err.message);
+          setError(true);
+        });
     }
   };
 
-  // if(Redirect){
+  const handleOpenModal = () => {
+    setError(true);
+  };
 
-  // }
+  const handleCloseModal = () => {
+    setError(false);
+  };
+
+  if (redirect) {
+    return <Redirect to="/otopark/superadmin/dashboard" />;
+  }
 
   return (
-    <>
+    <div>
+      {error && (
+        <LoginModal
+          isOpen={error}
+          openModal={handleOpenModal}
+          closeModal={handleCloseModal}
+          message={errorMessage}
+        />
+      )}
       <div
         style={{
-          // padding:'40px',
           height: "100vh",
           backgroundColor: "#00BBDC",
           backgroundImage: require("../../assets/images/LoginPageImg.svg")
@@ -80,34 +128,48 @@ export default function SignIn() {
                       backgroundColor: "#fff",
                       color: "1px solid #1717172E"
                     }}
-                    inputRef={register}
-                    className="textbox"
+                    inputRef={register({ required: true, maxLength: 30 })}
+                    className="mobile"
                     variant="outlined"
                     margin="normal"
-                    required
+                    // required
                     fullWidth
-                    id="email"
+                    id="mobile"
                     // label="Username or email"
-                    name="email"
-                    autoComplete="email"
-                    placeholder="Username or email"
+                    name="mobile"
+                    placeholder="Mobile Number"
                     autoFocus
                     InputProps={{
                       startAdornment: (
                         <InputAdornment position="start">
-                          <MailOutlineIcon />
+                          <PhoneAndroidIcon />
                         </InputAdornment>
                       )
                     }}
                   />
+                  {errors.mobile && errors.mobile.type === "required" && (
+                    <div
+                      className="container"
+                      style={{ margin: "10px", color: "red" }}
+                    >
+                      <div className="row">
+                        <i
+                          class="fa fa-exclamation-triangle"
+                          aria-hidden="true"
+                          style={{ margin: "5px" }}
+                        ></i>
+                        <span>Mobile No can not be Empty</span>
+                      </div>
+                    </div>
+                  )}
                   <TextField
                     style={{ backgroundColor: "#fff" }}
                     size="small"
-                    inputRef={register}
-                    className="textbox"
+                    inputRef={register({ required: true, maxLength: 30 })}
+                    className="password"
                     variant="outlined"
                     margin="normal"
-                    required
+                    // required
                     fullWidth
                     name="password"
                     // label="Password"
@@ -123,6 +185,21 @@ export default function SignIn() {
                       )
                     }}
                   />
+                  {errors.password && errors.password.type === "required" && (
+                    <div
+                      className="container"
+                      style={{ margin: "10px", color: "red" }}
+                    >
+                      <div className="row">
+                        <i
+                          class="fa fa-exclamation-triangle"
+                          aria-hidden="true"
+                          style={{ margin: "5px" }}
+                        ></i>
+                        <span>Password cannot be Empty</span>
+                      </div>
+                    </div>
+                  )}
                   {/* new */}
                   <div className={classes.forgotcheck}>
                     <div>
@@ -196,6 +273,6 @@ export default function SignIn() {
           />
         </footer>
       </div>
-    </>
+    </div>
   );
 }
